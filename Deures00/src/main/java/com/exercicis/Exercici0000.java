4210689e1f91668b564653f59d4e91c94c76e736
Bermu <alexbermudezcarmona@gmail.com>
2024-12-17 20:22:37 +0100

    alguns cambis

--- Deures00/src/main/java/com/exercicis/Exercici0000.java
+++ Deures00/src/main/java/com/exercicis/Exercici0000.java
@@ -6,9 +6,15 @@ public class Exercici0000 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        /*
-            TODO: Resol aquí l'exercici
-         */
+        System.out.print("Escriu el primer número: ");
+        Integer X = scanner.nextInt();
+        
+        System.out.print("Escriu el segon número: ");
+        Integer Y = scanner.nextInt();
+
+        Integer Z = X - Y;
+
+        System.out.println("El resultat de calcular "+X+" - "+Y+" és "+Z);
 
         scanner.close();
     }
