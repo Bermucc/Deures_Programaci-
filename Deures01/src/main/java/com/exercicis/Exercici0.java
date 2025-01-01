432b9e766824af6e6728a4e11e60c16031fa8560
Bermu <alexbermudezcarmona@gmail.com>
2025-01-01 20:48:45 +0100

    modificarClient

--- Deures01/src/main/java/com/exercicis/Exercici0.java
+++ Deures01/src/main/java/com/exercicis/Exercici0.java
@@ -423,8 +423,18 @@ public class Exercici0 {
      * @test ./runTest.sh "com.exercicis.TestExercici0#testModificarClient"
      */
     public static String modificarClient(String clauClient, String camp, Object nouValor) {
-        // TODO
-        return "";
+        if (!clients.containsKey(clauClient)) {
+            return "Client '"+ clauClient + " no existeix.";
+        }
+
+        HashMap<String, Object> client = (HashMap<String, Object>) clients.get(clauClient);
+        if (!client.containsKey(camp)){
+            return "El camp " + camp + " no existeix.";
+        }
+
+        client.put(camp, nouValor);
+
+        return "OK";
     }
 
     /**
