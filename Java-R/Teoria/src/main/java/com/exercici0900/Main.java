4175f81c4b1f76455d51840964b8eed03623b3bd
Bermu <156939828+Bermucc@users.noreply.github.com>
2025-01-08 14:17:40 +0100

    Update Main.java

--- Java-R/Teoria/src/main/java/com/exercici0900/Main.java
+++ Java-R/Teoria/src/main/java/com/exercici0900/Main.java
@@ -1,6 +1,11 @@
+// PRACTICANDO EN CASA [08/01/2025]
 package com.exercici0900;
 
 public class Main {
+    
+    public boolean isAnagrama(String palabra1 , String palabra2) {
+        
+    }
     public static void main(String[] args) {
         
         /* TODO: 
