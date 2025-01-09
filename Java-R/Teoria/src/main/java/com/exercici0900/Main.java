c71481569b8cb0d733647f8a47cea56a8fb7e7d4
Bermu <156939828+Bermucc@users.noreply.github.com>
2025-01-09 20:20:54 +0100

    He creado los 3 menus y se muestran en pantalla

--- Java-R/Teoria/src/main/java/com/exercici0900/Main.java
+++ Java-R/Teoria/src/main/java/com/exercici0900/Main.java
@@ -1,15 +1,48 @@
 // PRACTICANDO EN CASA [08/01/2025]
 package com.exercici0900;
 
+import java.util.Scanner;
+
 public class Main {
     
     public boolean isAnagrama(String palabra1 , String palabra2) {
-        
+        // Revisa que los inputs se ingrese algo.
+        if (palabra1 == null || palabra2 == null) {
+            return false;
+        }
+
+        // Pasamos las palabras, en minus y sin espacios.
+        palabra1 = palabra1.replaceAll("\\s", "").toLowerCase();
+        palabra2 = palabra2.replaceAll("\\s", "").toLowerCase();
+
+        // Revisamos las longitudes de ambas palabras
+        if (palabra1.length() != palabra2.length()) {
+            return false;
+        }
+
+        // Separa las palabras en caracteres
+        char[] char1 = palabra1.toCharArray();
+        char[] char2 = palabra2.toCharArray();
+
+        // Ordenamos los caracteres
+        java.util.Arrays.sort(char1);
+        java.util.Arrays.sort(char2);
+
+        // Comprueba si son iguales y devuelve "True" o "False"
+        return java.util.Arrays.equals(char1, char2);
     }
     public static void main(String[] args) {
         
-        /* TODO: 
-           Fes aquí el codi de l'exercici
-        */
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Escribe la primera palabra: ");
+        String palabra1 = scanner.nextLine();
+
+        System.out.println("Escribe la segunda palabra: ");
+        String palabra2 = scanner.nextLine();
+
+        System.out.println("¿Son las palabras " + palabra1 + " i " + palabra2 + " anagramas?: ");
+
+        
     }
 }
